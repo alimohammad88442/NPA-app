96a8092519cdbb67e9e5f56e073836e9c1a0263c
alimohammad88442 <alimohammad88442@gmail.com>
2023-11-19 12:04:28 +0300

    added searching by name for the players

--- lib/services/search_for_player_service.dart
+++ lib/services/search_for_player_service.dart
@@ -3,8 +3,8 @@ import 'package:nba_app/models/all_players_model.dart';
 
 class SearchForPlayerService {
   Future<PlayersModel> searchForPlayer(String id) async {
-    var data = await Api().get('https://balldontlie.io/api/v1/players/$id');
-    PlayersModel searchForPlayer = PlayersModel.fromjson(data);
+    var data = await Api().get('https://balldontlie.io/api/v1/players?search=$id');
+    PlayersModel searchForPlayer = PlayersModel.fromjson(data['data'][0]);
     return searchForPlayer;
   }
 }
